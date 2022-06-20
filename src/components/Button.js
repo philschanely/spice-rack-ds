1ab9c1ba91e407a4d84134a18725ed420b9e74b3
Phil Schanely <philschanely@gmail.com>
2022-06-20 19:44:23 -0400

    v0.1.2
    Add styled components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,41 @@
 import PropTypes from "prop-types";
- 
-const styles = {
-  background: "#fff",
-  border: "0.2rem solid #0099ff",
-  color: "#0099ff",
-  fontWeight: "bold",
-  letterSpacing: "0.1em",
-  padding: "1em",
-  textTransform: "uppercase"
+import styled from "styled-components";
+
+const Tokens = {
+  color: {
+    primary: '#0099ff',
+    white: '#fff',
+    neutral: 'grey',
+  }
 };
- 
+
 export const Button = ({
   children,
   disabled,
   onClick,
-}) => (
-  <button disabled={(disabled ? "true" : "")} onClick={onClick} style={styles}>
-    {children}
-  </button>
-);
- 
+}) => {
+  const buttonType = ({ disabled }) => `
+    text-transform: uppercase;
+    letter-spacing: 0.1em;
+    font-weight: bold;
+    color: ${disabled ? Tokens.color.neutral : Tokens.color.primary};
+  `;
+
+  const Root = styled.button`
+    background: ${Tokens.color.white};
+    border: 0.2rem solid ${disabled ? Tokens.color.neutral : Tokens.color.primary};
+    padding: 1em;
+    cursor: ${disabled ? 'not-allowed' : 'pointer'};
+    ${buttonType({ disabled })}
+  `;
+
+  return (
+    <Root disabled={disabled} onClick={onClick}>
+      {children}
+    </Root>
+  );
+};
+
 Button.propTypes = {
   children: PropTypes.node,
   disabled: PropTypes.bool,
